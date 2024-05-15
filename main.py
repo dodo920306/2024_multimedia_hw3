a816eb92afb9b53e0ed27e7734772a6abaa49e96
dodo920306 <dodo920306@gmail.com>
2024-05-15 20:47:05 +0800

    Finish codes and docs

--- main.py
+++ main.py
@@ -1,39 +1,16 @@
-# 2024 Multimedia HW3
-
-## Prerequisites
-Make sure that you have python and pip installed on your computer.
-
-Run
-```bash
-$ python3 -m pip install -r requirements.txt
-```
-to get all you need to run the code.
-
-## Usage
-Simply run
-```bash
-$ python3 main.py <image file path>
-```
-to get the results.
-
-A sample image file `Histogram+Edge.bmp` in this directory is used as the example for the following description.
-
-## Histogram Equalization
-The main package used here to compute image data is `opencv`.
-
-```python
 from cv2 import imread, cvtColor, COLOR_BGR2RGB, COLOR_BGR2GRAY, COLOR_GRAY2RGB, GaussianBlur, imshow, normalize, NORM_MINMAX, threshold, THRESH_BINARY, namedWindow, createTrackbar, imwrite
 from sys import argv
 from matplotlib.pyplot import subplots, tight_layout, savefig, show
 from numpy import histogram, array, zeros, zeros_like, where, sum
 from numpy.ma import masked_equal, filled
-```
 
-Methods are detailed below.
+def apply_threshold(lb):
+    global img
+    _, timg = threshold(img, lb, 255, THRESH_BINARY)
+    imshow('Edge Detection', timg)
+    imwrite('result2_without_turn_it_binary.png', img)
 
-First, I simply read in the input image, show it, convert it to grayscale, and make the histogram of it.
-
-```python
+if __name__ == '__main__':
     cimg = imread(argv[1])
     assert cimg is not None, "file could not be read, check with os.path.exists()"
 
@@ -53,11 +30,8 @@ First, I simply read in the input image, show it, convert it to grayscale, and m
     axes[1][0].set_xlabel('Gray Scale')
     axes[1][0].set_ylabel('Frequency')
     axes[1][0].set_title('Histogram of Original Grayscale')
-```
 
-Then, I apply the Histogram Equalization.
-
-```python
+    # Histogram Equalization
     cdf = y.cumsum() # cumulative sum
     # For each cdf element, multiply it with the maximum of y, and divide it with the sum of the image.
     cdf_normalized = cdf * float(y.max()) / cdf.max()
@@ -80,21 +54,6 @@ Then, I apply the Histogram Equalization.
     axes[1][1].set_ylabel('Frequency')
     axes[1][1].set_title('Histogram of Equalized')
 
-    ...
-
-    tight_layout()
-    show()
-```
-
-The result looks like this
-
-![](result1.png)
-
-## Edge Detection
-
-After the image was histogram equalized, I apply Edge Detection by smoothing it, convolute it with sobel x and sobel y matrices, and normalize it.
-
-```python
     # Edge Detection
     sobel_x = array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
     sobel_y = sobel_x.T
@@ -111,24 +70,10 @@ After the image was histogram equalized, I apply Edge Detection by smoothing it,
             res[x, y] = (res_x + res_y) ** 0.5
 
     img = normalize(where(res > 255, 255, res).astype('uint8'), None, 0, 255, NORM_MINMAX)
-```
+    namedWindow('Edge Detection')
+    createTrackbar('Lower Bound:', 'Edge Detection', 0, 255, apply_threshold)
+    apply_threshold(0)
 
-![](result2_without_turn_it_binary.png)
-
-That looks cool, right? But it looks blurry, and edges shouldn't be blurry, so I turn it binary.
-
-```python
-def apply_threshold(lb):
-    global img
-    _, timg = threshold(img, lb, 255, THRESH_BINARY)
-    imshow('Edge Detection', timg)
-    # imwrite('result2.png', timg)
-```
-
-At this point, the only thing that is left to do is give it a threshold. Otherwise, it could look messy since all positive values are white.
-
-![](result2_when_lower_bound_is_0.png)
-
-Don't worry. A track bar is provided below the picture on the window so you can slide it however you like to get the best result for you.
-
-![](result2.png)
+    tight_layout()
+    savefig("result1.png")
+    show()
